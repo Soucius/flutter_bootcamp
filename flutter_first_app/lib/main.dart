363900c004e5bed22b11cc353ff36029ad48b645
Soucius <soucius.ggwp@gmail.com>
2024-08-07 00:27:26 +0300

    floating action button and material design rules

--- flutter_first_app/lib/main.dart
+++ flutter_first_app/lib/main.dart
@@ -8,12 +8,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.teal),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("soucius"),
-          backgroundColor: Colors.amber,
+          title: Text("soucius appbar"),
+          backgroundColor: Colors.teal,
         ),
-        body: Text("soucius"),
+        body: Text(
+          "soucius body",
+          style: TextStyle(
+              color: Colors.red, fontSize: 24, fontWeight: FontWeight.w500),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            debugPrint("clicked");
+          },
+          child: Icon(
+            Icons.account_circle_rounded,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.red,
+        ),
       ),
     );
   }
