63d9ebd9c3c425cd3f8486b336bc4fedaddd6460
Soucius <soucius.ggwp@gmail.com>
2024-08-09 12:59:09 +0300

    itemBuilder and seperatorBuilder

--- flutter_scrolls_app/lib/listview.dart
+++ flutter_scrolls_app/lib/listview.dart
@@ -4,7 +4,7 @@ class Listview extends StatelessWidget {
   Listview({super.key});
 
   List<Student> allStudents = List.generate(
-      500,
+      50,
       (index) => Student(index + 1, "student name ${index + 1}",
           "student lastname ${index + 1}"));
 
@@ -12,19 +12,80 @@ class Listview extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Studend List"),
+        title: Text("Student List"),
       ),
-      body: ListView(
-        children: allStudents
-            .map((Student student) => ListTile(
-                  title: Text(student.name),
-                  subtitle: Text(student.lastname),
-                  leading: CircleAvatar(
-                    child: Text(student.id.toString()),
-                  ),
-                ))
-            .toList(),
-      ),
+      body: listviewSeperated(),
+    );
+  }
+
+  ListView listviewSeperated() {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+        var nowStudent = allStudents[index];
+
+        return Card(
+          color:
+              index % 2 == 0 ? Colors.orange.shade100 : Colors.purple.shade100,
+          child: ListTile(
+            onTap: () {
+              print("element clicked: ${index + 1}");
+            },
+            title: Text(nowStudent.name),
+            subtitle: Text(nowStudent.lastname),
+            leading: CircleAvatar(
+              child: Text(nowStudent.id.toString()),
+            ),
+          ),
+        );
+      },
+      itemCount: allStudents.length,
+      separatorBuilder: (BuildContext context, int index) {
+        if ((index + 1) % 4 == 0) {
+          return Divider(
+            thickness: 1,
+            color: Colors.teal,
+          );
+        }
+        return SizedBox();
+      },
+    );
+  }
+
+  ListView listviewBuilder() {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        var nowStudent = allStudents[index];
+
+        return Card(
+          color:
+              index % 2 == 0 ? Colors.orange.shade100 : Colors.purple.shade100,
+          child: ListTile(
+            onTap: () {
+              print("element clicked: ${index + 1}");
+            },
+            title: Text(nowStudent.name),
+            subtitle: Text(nowStudent.lastname),
+            leading: CircleAvatar(
+              child: Text(nowStudent.id.toString()),
+            ),
+          ),
+        );
+      },
+      itemCount: allStudents.length,
+    );
+  }
+
+  ListView classicListview() {
+    return ListView(
+      children: allStudents
+          .map((Student student) => ListTile(
+                title: Text(student.name),
+                subtitle: Text(student.lastname),
+                leading: CircleAvatar(
+                  child: Text(student.id.toString()),
+                ),
+              ))
+          .toList(),
     );
   }
 }
