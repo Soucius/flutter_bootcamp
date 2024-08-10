3a9a14d0f8d4353e5d673f1db9aad59b2a745214
Soucius <soucius.ggwp@gmail.com>
2024-08-10 22:48:41 +0300

    SliverList and static elements

--- flutter_scrolls_app/lib/custom_scroll_and_slivers.dart
+++ flutter_scrolls_app/lib/custom_scroll_and_slivers.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class CustomScrollAndSlivers extends StatelessWidget {
   const CustomScrollAndSlivers({super.key});
+  final String _img =
+      "https://arabam-blog.mncdn.com/wp-content/uploads/2022/01/A-Red-The-Ducati-1098.jpg";
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +18,121 @@ class CustomScrollAndSlivers extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
-            title: Text("sliver app bar"),
+            // title: Text("sliver app bar"),
             backgroundColor: Colors.red,
-            expandedHeight: 200,
+            expandedHeight: 300,
             floating: true,
+            snap: true,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("sliver app bar"),
+              centerTitle: true,
+              background: Image.network(
+                _img,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
+          SliverList(
+            delegate: SliverChildListDelegate(staticListElements),
+          )
         ],
       ),
     );
+  }
+
+  List<Widget> get staticListElements {
+    return [
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 1",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.amber,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 2",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.teal,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 3",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.blue,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 4",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.orange,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 5",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.purple,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 6",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.cyan,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 7",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.orange,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 8",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.purple,
+        alignment: Alignment.center,
+      ),
+      Container(
+        height: 100,
+        child: Text(
+          "static list element 9",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+        color: Colors.cyan,
+        alignment: Alignment.center,
+      ),
+    ];
   }
 }
