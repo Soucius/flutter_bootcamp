c46aaad8a334ce3d1cd234891a3225451095b95a
Soucius <soucius.ggwp@gmail.com>
2024-08-11 11:57:17 +0300

    WillPopScope

--- flutter_navigations/lib/red_page.dart
+++ flutter_navigations/lib/red_page.dart
@@ -8,29 +8,41 @@ class RedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("red page"),
-        backgroundColor: Colors.red,
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "red page",
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                _randomNum = Random().nextInt(100);
-                print("created num: $_randomNum");
-                Navigator.of(context).pop(_randomNum);
-              },
-              child: Text("go back"),
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: () {
+        print("onwillpop worked");
+
+        if (_randomNum == 0) {
+          _randomNum = Random().nextInt(100);
+          Navigator.pop(context, _randomNum);
+        }
+
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("red page"),
+          backgroundColor: Colors.red,
+          // automaticallyImplyLeading: false,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "red page",
+                style: TextStyle(fontSize: 24),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _randomNum = Random().nextInt(100);
+                  print("created num: $_randomNum");
+                  Navigator.of(context).pop(_randomNum);
+                },
+                child: Text("go back"),
+              ),
+            ],
+          ),
         ),
       ),
     );
